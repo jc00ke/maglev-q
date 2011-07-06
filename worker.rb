78e0007654c5b386318e847686da7971ed55e51f
jc00ke <jesse@jc00ke.com>
2011-07-05 19:17:41 -0700

    Adding rescue to catch failed jobs

--- worker.rb
+++ worker.rb
@@ -1,7 +1,16 @@
 work = "let's do something"
 while work do
-  Maglev.abort_transaction
-  work = Maglev::PERSISTENT_ROOT[:q].shift
-  Maglev.commit_transaction
-  work.call if work
+  begin
+    Maglev.abort_transaction
+    work = Maglev::PERSISTENT_ROOT[:q].shift
+    nap_time = rand(3)
+    puts "\n\n\n"
+    puts "sleeping for #{nap_time} seconds after de-queing"
+    sleep nap_time
+    Maglev.commit_transaction
+    work.call if work
+  rescue Maglev::CommitFailedException
+    puts "job failed :-( REDO!"
+    redo
+  end
 end
