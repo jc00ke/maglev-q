45f248e542ba44b822119f3f1fa963dd87f1db7f
jc00ke <jesse@jc00ke.com>
2011-07-05 20:04:37 -0700

    Remove puts so we avoid commit exception

--- worker.rb
+++ worker.rb
@@ -3,10 +3,6 @@ while work do
   begin
     Maglev.abort_transaction
     work = Maglev::PERSISTENT_ROOT[:q].shift
-    nap_time = rand(3)
-    puts "\n\n\n"
-    puts "sleeping for #{nap_time} seconds after de-queing"
-    sleep nap_time
     Maglev.commit_transaction
     work.call if work
   rescue Maglev::CommitFailedException
