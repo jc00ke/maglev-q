3ba830abe11b4a0ebf9d735f55f13db8680bd86d
jc00ke <jesse@jc00ke.com>
2011-07-05 18:54:54 -0700

    Print more son

--- queue.rb
+++ queue.rb
@@ -1,9 +1,9 @@
 Maglev.abort_transaction
 
-Maglev::PERSISTENT_ROOT[:q] = []
+Maglev::PERSISTENT_ROOT[:q] ||= []
 
 20.times do |t|
-  Maglev::PERSISTENT_ROOT[:q] << Proc.new { sleep 1; puts t }
+  Maglev::PERSISTENT_ROOT[:q] << Proc.new { sleep 1; puts "job ##{t}" }
 end
 
 Maglev.commit_transaction
